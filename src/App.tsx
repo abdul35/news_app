0589e71464d906886d86c7ed329dcf2d117c91e6
abdul35 <46883525+abdul35@users.noreply.github.com>
2022-11-11 16:03:22 +0000

    Initialazed react router navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 import "./App.css";
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>What's up Duuude</h1>
-			</header>
-		</div>
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<h1>What's up Duuude</h1>
+					<nav>
+						<ul>
+							<li>
+								{/* <Link to="/">Home</Link> */}
+								<a href="/">h</a>
+							</li>
+							<li>
+								<a href="/about">a</a>
+								{/* <Link to="/about">About</Link> */}
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<Switch>
+					<Route path="/about" exact>
+						<About />
+					</Route>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
+	
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return ( 
+        <>
+            
+            <h1>Index page</h1>
+
+        </>
+    );
+}
+
+export default Home;
